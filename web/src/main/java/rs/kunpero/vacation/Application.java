599233d6df2e47794e2691c6c45fd0875242414f
kunpero <kunperooo@gmail.com>
2019-10-08 21:17:39 +0300

    application refactoring

--- web/src/main/java/rs/kunpero/vacation/Application.java
+++ web/src/main/java/rs/kunpero/vacation/Application.java
@@ -2,10 +2,12 @@ package rs.kunpero.vacation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 
 @SpringBootApplication(exclude = {JacksonAutoConfiguration.class})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
 }
