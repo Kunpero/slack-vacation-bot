6e6c631d72097415f00b666704cc88644510a47e
kunpero <kunperooo@gmail.com>
2019-10-12 19:54:29 +0300

    add error response

--- web/src/main/java/rs/kunpero/vacation/api/VacationController.java
+++ web/src/main/java/rs/kunpero/vacation/api/VacationController.java
@@ -2,10 +2,18 @@ package rs.kunpero.vacation.api;
 
 import com.github.seratch.jslack.Slack;
 import com.github.seratch.jslack.api.methods.SlackApiException;
+import com.github.seratch.jslack.api.model.block.LayoutBlock;
+import com.github.seratch.jslack.api.model.block.SectionBlock;
+import com.github.seratch.jslack.api.model.block.composition.MarkdownTextObject;
+import com.github.seratch.jslack.api.model.view.View;
+import com.github.seratch.jslack.api.model.view.ViewState;
+import com.github.seratch.jslack.api.model.view.ViewSubmit;
+import com.github.seratch.jslack.api.model.view.ViewTitle;
 import com.github.seratch.jslack.app_backend.interactive_messages.payload.BlockActionPayload;
 import com.github.seratch.jslack.app_backend.interactive_messages.payload.PayloadTypeDetector;
 import com.github.seratch.jslack.app_backend.slash_commands.response.SlashCommandResponse;
 import com.github.seratch.jslack.app_backend.views.payload.ViewSubmissionPayload;
+import com.github.seratch.jslack.app_backend.views.response.ViewSubmissionResponse;
 import com.google.gson.Gson;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,11 +21,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import rs.kunpero.vacation.api.dto.FormActivity;
 import rs.kunpero.vacation.service.VacationService;
 import rs.kunpero.vacation.service.dto.AddVacationInfoRequestDto;
 import rs.kunpero.vacation.service.dto.AddVacationInfoResponseDto;
 import rs.kunpero.vacation.util.ActionId;
+import rs.kunpero.vacation.util.BlockId;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -26,9 +34,7 @@ import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
 import java.util.List;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
-import java.util.function.BiConsumer;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import static org.springframework.http.MediaType.APPLICATION_FORM_URLENCODED_VALUE;
@@ -37,6 +43,10 @@ import static rs.kunpero.vacation.util.ActionId.ADD_VACATION;
 import static rs.kunpero.vacation.util.ActionId.SET_FROM;
 import static rs.kunpero.vacation.util.ActionId.SET_SUBSTITUTION;
 import static rs.kunpero.vacation.util.ActionId.SET_TO;
+import static rs.kunpero.vacation.util.BlockId.DATE_FROM;
+import static rs.kunpero.vacation.util.BlockId.DATE_TO;
+import static rs.kunpero.vacation.util.BlockId.ERROR;
+import static rs.kunpero.vacation.util.BlockId.SUBSTITUTION;
 import static rs.kunpero.vacation.util.ViewHelper.ADD_VACATION_INFO_VIEW;
 import static rs.kunpero.vacation.util.ViewHelper.START_MENU;
 
@@ -44,7 +54,6 @@ import static rs.kunpero.vacation.util.ViewHelper.START_MENU;
 @Slf4j
 @RequestMapping("/vacation")
 public class VacationController {
-    private static final ConcurrentMap<String, FormActivity> activityCache = new ConcurrentHashMap<>();
     private static final PayloadTypeDetector TYPE_DETECTOR = new PayloadTypeDetector();
 
     @Autowired
@@ -79,73 +88,49 @@ public class VacationController {
                         .view(ADD_VACATION_INFO_VIEW)
                         .triggerId(payload.getTriggerId()));
             }
-            if (actionId == SET_FROM) {
-                String viewId = payload.getView().getId();
-                FormActivity formActivity = activityCache.getOrDefault(viewId, new FormActivity());
 
-                if (validateDate(LocalDate.parse(actions.get(0).getSelectedDate()), formActivity.getDateTo())) {
-
-                }
-
-                fillActivityCache(viewId, actions.get(0).getSelectedDate(),
-                        (form, value) -> form.setDateFrom(LocalDate.parse(value)));
-            }
-            if (actionId == SET_TO) {
-                String viewId = payload.getView().getId();
-                FormActivity formActivity = activityCache.getOrDefault(viewId, new FormActivity());
-
-                if (validateDate(formActivity.getDateFrom(), LocalDate.parse(actions.get(0).getSelectedDate()))) {
-
-                }
-
-                fillActivityCache(viewId, actions.get(0).getSelectedDate(),
-                        (form, value) -> form.setDateTo(LocalDate.parse(value)));
-            }
-            if (actionId == SET_SUBSTITUTION) {
-                String viewId = payload.getView().getId();
-                fillActivityCache(viewId, actions.get(0).getSelectedUsers(),
-                        FormActivity::setSubstitution);
-            }
-        }
-
-        if ("view_submission".equals(type)) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else if ("view_submission".equals(type)) {
             ViewSubmissionPayload payload = gson.fromJson(body, ViewSubmissionPayload.class);
-            log.info(activityCache.toString());
-            /** maybe need to close if error occur. conduct research*/
-            FormActivity formActivity = activityCache.get(payload.getView().getId());
+            Map<String, Map<String, ViewState.Value>> valuesMap = payload.getView().getState().getValues();
             AddVacationInfoRequestDto requestDto = new AddVacationInfoRequestDto()
                     .setUserId(payload.getUser().getId())
-                    .setDateFrom(formActivity.getDateFrom())
-                    .setDateTo(formActivity.getDateTo())
-                    .setSubstitutionIdList(formActivity.getSubstitution());
-
+                    .setTeamId(payload.getUser().getTeamId())
+                    .setDateFrom(LocalDate.parse(valuesMap.get(DATE_FROM.name()).get(SET_FROM.name()).getSelectedDate()))
+                    .setDateTo(LocalDate.parse(valuesMap.get(DATE_TO.name()).get(SET_TO.name()).getSelectedDate()))
+                    .setSubstitutionIdList(valuesMap.get(SUBSTITUTION.name()).get(SET_SUBSTITUTION.name()).getSelectedUsers());
             AddVacationInfoResponseDto responseDto = vacationService.addVacationInfo(requestDto);
-            if (responseDto.isSuccesful()) {
-                activityCache.remove(payload.getView().getId());
+            if (!responseDto.isSuccesful()) {
+                buildErrorResponse(payload, responseDto.getErrorDescription(), response);
             }
-
-            // TODO: return error to slack
         }
-
-        if ("view_closed".equals(type)) {
-            ViewSubmissionPayload payload = gson.fromJson(body, ViewSubmissionPayload.class);
-            activityCache.remove(payload.getView().getId());
-        }
-
-        response.setStatus(HttpServletResponse.SC_OK);
     }
 
-    private boolean validateDate(LocalDate dateFrom, LocalDate dateTo) {
-        if (dateFrom == null || dateTo == null) {
-            return false;
+    private void buildErrorResponse(ViewSubmissionPayload payload, String errorDescription, HttpServletResponse response) throws IOException {
+        List<LayoutBlock> blocks = payload.getView().getBlocks();
+        if (blocks.size() == BlockId.values().length) {
+            blocks.remove(BlockId.values().length - 1);
         }
-        return dateFrom.isBefore(dateTo);
-    }
-
-    private <T> void fillActivityCache(String viewId, T value, BiConsumer<FormActivity, T> fieldConsumer) {
-        log.trace("ViewId={}", viewId);
-        FormActivity formActivity = activityCache.getOrDefault(viewId, new FormActivity());
-        fieldConsumer.accept(formActivity, value);
-        activityCache.put(viewId, formActivity);
+        blocks.add(SectionBlock.builder()
+                .blockId(ERROR.name())
+                .text(MarkdownTextObject.builder()
+                        .text(String.format("`%s`", errorDescription))
+                        .build()).build());
+        View viewWithError = View.builder()
+                .type("modal")
+                .callbackId("callback_id")
+                .title(ViewTitle.builder().type("plain_text").text("New vacation info").build())
+                .submit(ViewSubmit.builder().type("plain_text").text("Submit").build())
+                .notifyOnClose(true)
+                .blocks(blocks)
+                .build();
+        ViewSubmissionResponse submissionResponse = ViewSubmissionResponse.builder()
+                .responseAction("update")
+                .view(viewWithError)
+                .build();
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(gson.toJson(submissionResponse));
+        response.getWriter().flush();
     }
 }
