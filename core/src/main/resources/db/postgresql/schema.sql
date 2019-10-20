7fccf5b0d97a1c8ecc1939831986d5a1731807cc
kunpero <kunperooo@gmail.com>
2019-10-20 18:11:43 +0300

    add postgres

---
+++ core/src/main/resources/db/postgresql/schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE VACATION_INFO (
+ID                      BIGINT              PRIMARY KEY,
+USER_ID                 VARCHAR (21)        NOT NULL,
+TEAM_ID                 VARCHAR (21)        NOT NULL,
+DATE_FROM               DATE                NOT NULL,
+DATE_TO                 DATE                NOT NULL,
+SUBSTITUTION_USER_IDS   VARCHAR (512)
+);
+CREATE SEQUENCE IF NOT EXISTS SEQ_VACATION_INFO START WITH 1 INCREMENT BY 1;
